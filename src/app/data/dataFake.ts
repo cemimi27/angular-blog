cc32de136a394fa2061540c450a394e9c878cf9b
cemimi27 <carloseduardomimi@gmail.com>
2023-12-09 13:48:48 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title": "Saiu a nova versão do Angular",
+        "description": "Nesta sexta-feira (09), a Google acaba de soltar a nova versão do FrameWork Angular",
+        "photoCover": "https://angular.io/assets/images/logos/angular/angular.svg",
+    },
+    {
+        "id":"2",
+        "title": "Lucas entra na mira de quatro clubes, mas São Paulo mantém otimismo por renovação",
+        "description": "Lucas com frescura para renovar com o São Paulo",
+        "photoCover": "https://s2.glbimg.com/iG2_0IKKmgSOqkC7DF153-dfRWY=/540x304/top/smart/https://i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2023/U/L/NzfxD1QGyUg2ShCABuUw/lucas.jpg",
+    },
+    {
+        "id":"3",
+        "title": "São Paulo recebe proposta de quase R$ 100 milhões do Zenit por Beraldo",
+        "description": "Beraldo morrendo de rir ao reber uma grande proposta para a próxima temporada",
+        "photoCover": "https://s2.glbimg.com/IuU9DcM-pDLBwVCi5O8nN9ZfD80=/540x304/top/smart/https://i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2023/A/l/DP2GCvSy2xEOa3lM040w/f2n-ticwaaaovqv.jpg",
+    },
+    {
+        "id":"4",
+        "title": "Expectativa x realidade: como os primeiros meses de James no São Paulo deixam dúvida sobre futuro",
+        "description": "lorem30",
+        "photoCover": "https://s2.glbimg.com/xhQqK9P-tB_rWPd-4fNjPG7_U-U=/540x304/top/smart/https://i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2023/a/y/VYF8uORj2VWGDdvsZItw/rib3425.jpg",
+    }
+];
